d95b6345009c825868b328c2cedaac3c68d34aba
Nekkar-R <rin.nsk.3110@gmail.com>
2026-02-24 23:22:51 +0900

    add

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -2,7 +2,7 @@ local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 -- Move to previous/next
-map("n", "<C-h>", "<Cmd>BufferPrevious<CR>", opts) --Ctrl+jで前のBufferに移動
-map("n", "<C-l>", "<Cmd>BufferNext<CR>", opts) --Ctrl+kで次のBufferに移動
+map("n", "<S-h>", "<Cmd>BufferLineCycleNext<CR>", opts)
+map("n", "<S-l>", "<Cmd>BufferLineCyclePrev<CR>", opts)
 -- Close buffer
-map("n", "<leader>bd", "<Cmd>BufferClose<CR>", opts) --<leader>+b+dでBufferを削除
+map("n", "<leader>bd", "<Cmd>bdelete<CR>", opts)
