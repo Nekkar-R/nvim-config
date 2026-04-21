cf86547b3f0f89023d5deb589abdeeb9a696505e
Nekkar <rin.nsk.3110@gmail.com>
2026-04-21 11:43:33 +0900

    remove

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -33,28 +33,13 @@ require("lazy").setup({
 
 		-- 見た目系.
 		{ import = "plugins.themes" }, -- themes.
-		-- { import = "plugins.noice" }, -- 通知リッチにするやつ.
-		-- { import = "plugins.alpha-nvim" }, -- 起動したときに出るやーつ.
-		-- { import = "plugins.lualine" }, -- 下に出てくるかっこいいやつ.
-		-- { import = "plugins.bufferline" }, -- buffer表示するやーつ.
-		-- { import = "plugins.hlchunk" }, -- インデント見やすくするやつ.
 		{ import = "plugins.toggleterm" }, -- ポップアップウィンドウ的な.
 
 		-- 補完系.
-		-- { import = "plugins.lsp" }, -- lsp関連のやつ 調整中 誰か助けて().
-		-- { import = "plugins.cmp" }, -- cmp 要するに補完.
-		-- { import = "plugins.autopairs" }, -- 括弧とかの補完.
 		{ import = "plugins.surround" }, -- 色々括弧とかをいじれるやつ.
-		-- { import = "plugins.ts-autotag" }, -- タグを補完してくれるやつ.
 
 		-- 便利系.
 		{ import = "plugins.oil" }, -- ファイルビューア.
-		-- { import = "plugins.telescope" }, -- ファイル検索.
-		-- { import = "plugins.treesitter" }, -- コードハイライト.
-		-- { import = "plugins.lazygit" }, -- lazygitのやーつ.
-		-- { import = "plugins.conform" }, -- コード綺麗にしてくれるやつ.
-		-- { import = "plugins.which_key" }, -- 右下に出てくるショートカットのヒント.
-
 		-- お遊び.
 		-- { import = "plugins.hellshake-yano" }, -- 移動が楽になるやつ.
 		{ import = "plugins.cord-nvim" }, -- Discordにいじってるファイル出るやつ.
